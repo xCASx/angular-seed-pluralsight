0ed589983c34e784a147c2a656dea75f393a2362
cas <cas-05@mail.ru>
2014-03-09 02:06:07 +0200

    Add filters.

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -2,10 +2,12 @@
 
 eventsApp.controller('EventController',
     function EventController($scope) {
+        $scope.sortorder = 'name';
         $scope.event = {
             name: 'Angular Boot Camp',
             date: '1/1/2013',
             time: '10:30 am',
+            cost: 34,
             location: {
                 address: 'Google Headquarters',
                 city: 'Mountain View',
@@ -16,7 +18,7 @@ eventsApp.controller('EventController',
                 {
                     name: 'Directives Masterclass',
                     creatorName: 'Bob Smith',
-                    duration: '1 hr',
+                    duration: 1,
                     level: 'Advanced',
                     abstract: 'In this session you will learn the ins and outs of directives!',
                     upVoteCount: 0
@@ -24,7 +26,7 @@ eventsApp.controller('EventController',
                 {
                     name: 'Scopes for fun and profit',
                     creatorName: 'John Doe',
-                    duration: '30 mins',
+                    duration: 2,
                     level: 'Introductory',
                     abstract: 'This session will take a closer look at scopes. Learn what they do, how they do it, and how to get them to do it for you.',
                     upVoteCount: 0
@@ -32,7 +34,7 @@ eventsApp.controller('EventController',
                 {
                     name: 'Well Behaved Controllers',
                     creatorName: 'John Doe',
-                    duration: '2 hours',
+                    duration: 4,
                     level: 'Intermediate',
                     abstract: 'Controllers are the beginning of everything Angular does. Learn how to craft controllers that will win the respect of your friends and neighbors.',
                     upVoteCount: 0
